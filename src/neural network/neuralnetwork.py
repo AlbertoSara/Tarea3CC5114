783adca78332c6840757f85a2b586a6ad10dc796
AlbertoSara <albsaraz@gmail.com>
2018-11-09 23:27:00 -0300

    reestructuracion codigo

--- src/neural network/neuralnetwork.py
+++ src/neural network/neuralnetwork.py
@@ -3,8 +3,6 @@
 
 import numpy as np
 
-
-
 class NeuralNetwork:
     def __init__(self, inp, outp, hidden_layers):
         self.input = inp           
@@ -20,13 +18,6 @@ class NeuralNetwork:
             self.errors[i] = np.zeros([self.errors[i-1].shape[1], hidden_layers[i-1]])   
         self.weights[len(self.weights) - 1] = np.random.rand(self.weights[len(self.weights) - 1].shape[1], self.y.shape[0])    
         self.errors[len(self.errors) - 1] = np.zeros([self.errors[len(self.errors) - 1].shape[1], self.y.shape[0]])    
-        print(self.weights)
-        print("---")
-        print(self.errors)
-        print("---")
-        print("---")
-        print("---")
-        print("---")
         self.layers = [0]*(len(hidden_layers)+2)
         self.layers[0] = np.copy(self.input)
         self.layers[len(self.layers) - 1] = self.output
@@ -39,30 +30,10 @@ class NeuralNetwork:
     def backpropagate(self, rate):
 
         self.errors[len(self.errors)-1] = self.layers[self.n_layer-1] - self.y
-        print("---")
-        print("---")
-        print("---")
-        print("layers")
-        print(self.layers)
-        print("---")
-        print("---")
-        print("---")
-        print("---")
-        print("---")
-        
-        print(self.errors)
-        print("---")
-        print("---")
-        print("---")
-        print("---")
-        print("---")
+
         for i in range(len(self.errors) - 2, 0, -1):
             self.errors[i] = np.dot(self.errors[i+1], self.weight[i+1]) *  self.sigmoid_deriv(self.layers[i])
-        print(self.errors)
-        print("---")
-        print(self.weights)
-        print("---")
-        print(self.layers)
+
         for i in range(len(self.errors) - 1):
             grad = np.dot(self.errors[i].T, self.layers[i+1].T) / self.input.shape[0]
             self.weights[i] = self.weights[i] - rate*grad
@@ -73,34 +44,40 @@ class NeuralNetwork:
     def sigmoid_deriv(self, x):
         return x * (1.0 - x)
 
- #   def backpropagation(self):
-        
- #       self.errors[]
-
     def train(self, e, rate):
         for i in range(e):
             self.feedforward()
-            self.backpropagation()
+            self.backpropagate(rate)
 
 
 if __name__ == "__main__":
-    X = np.array([[0,0,1],
-                  [0,1,1]])
-    
-    y = np.array([[0],[1],[1],[0]])
-    z = [3]
-    nn = NeuralNetwork(X,y, z)
 
-    for i in range(4):
-#        print(nn.layers)
-#        print("\n layers")
-#        print(nn.input)
-#        print("\n input")
-#        print(nn.output)
-#        print("\n output")
-#        print(nn.y)
-#        print("\n target")
-#        print(nn.weights)
-#        print("\n weights")
-        nn.feedforward()
-        nn.backpropagate(0.1)
+    inpt = np.zeros([1,42])
+    clss = np.zeros([1,3])
+    with open('data/connect-4.data', 'r') as f:
+        i = 0
+        for line in f:
+            i = i + 1
+            if i == 10:
+                break
+            l = line.replace("b","0").strip(' \t\n\r')
+            l = l.replace("x","1")
+            l = l.replace("o","2")
+            l = l.split(",")
+            numbers = np.array([list(map(int, l[0:42]))])
+            inpt = np.concatenate((numbers, inpt))   
+            if l[42] == "win":
+                clss = np.concatenate((np.array([[1,0,0]]),clss))
+            elif l[42] == "loss":
+                clss = np.concatenate((np.array([[0,1,0]]),clss))
+            elif l[42] == "draw":
+                clss = np.concatenate((np.array([[0,0,1]]),clss))
+
+    hidden = [3]
+    nn = NeuralNetwork(inpt, clss, hidden)
+
+    nn.train(100, 0.1)
+
+
+input("\n presione enter para continuar")
+#TODO no empezar las tareas el mismo dia de la entrega
